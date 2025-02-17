fb02302c01be671c7168226d80e176eaa53ac98f
Amar Harolikar <amar@harolikar.com>
2025-02-17 16:29:02 +0530

    removing of running mand

--- main.py
+++ main.py
@@ -183,6 +183,36 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
+# Hide Streamlit default UI elements
+hide_streamlit_style = """
+            <style>
+            div[data-testid="stToolbar"] {
+            visibility: hidden;
+            height: 0%;
+            position: fixed;
+            }
+            div[data-testid="stDecoration"] {
+            visibility: hidden;
+            height: 0%;
+            position: fixed;
+            }
+            div[data-testid="stStatusWidget"] {
+            visibility: hidden;
+            height: 0%;
+            position: fixed;
+            }
+            #MainMenu {
+            visibility: hidden;
+            height: 0%;
+            }
+            header {
+            visibility: hidden;
+            height: 0%;
+            }
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
 # Header
 st.markdown(
     """
